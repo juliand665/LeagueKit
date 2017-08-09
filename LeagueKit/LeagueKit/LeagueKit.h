7076aed3d17ccb42a6511a30b4d9d3a8d6e93d18
juliand665 <julian.dunskus@gmail.com>
2017-08-09 02:46:14 +0200

    minimized objc code
    Yes, I am this petty.

--- LeagueKit/LeagueKit/LeagueKit.h
+++ LeagueKit/LeagueKit/LeagueKit.h
@@ -1,12 +1,2 @@
-//
-//  LeagueKit.h
-//  LeagueKit
-//
-//  Created by Julian Dunskus on 13.06.17.
-//  Copyright © 2017 Julian Dunskus. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double LeagueKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char LeagueKitVersionString[];
+FOUNDATION_EXPORT double LeagueKitVersionNumber; FOUNDATION_EXPORT const unsigned char LeagueKitVersionString[];
