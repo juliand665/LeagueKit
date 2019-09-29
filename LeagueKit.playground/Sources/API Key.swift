6fcb4a557d8439297a1300162130542aca955fe3
Julian Dunskus <julian.dunskus@gmail.com>
2019-09-29 06:15:55 +0200

    replaced gitignore

--- LeagueKit.playground/Sources/API Key.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-/// - note: You should never expose your api key publicly, so make sure to only use this class from a server you control or in privately distributed apps.
-public let apiKey: String? = nil // insert your API key here for testing
